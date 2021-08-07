783437fad6aba47c4692105976f7c406bbe2a6c3
IamLakhan <gurbanilakhan7@gmail.com>
2021-08-07 13:41:05 +0530

    edited model class

--- account/models.py
+++ account/models.py
@@ -6,3 +6,6 @@ class Task(models.Model):
     description = models.TextField()
     is_done = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
