2dda9f2769f64bb700b082448eee161ba307d9c5
IamLakhan <gurbanilakhan7@gmail.com>
2021-08-07 14:06:09 +0530

    added add task functionality

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import login_user, logout_user, home_task
+from .views import login_user, logout_user, home_task, task_add
 
 urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('', home_task, name='home'),
+    path('add/', task_add),
 ]
--- account/views.py
+++ account/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from .models import Task
+
+import datetime
 # Create your views here.
 def login_user(request):
     if request.method == "GET":
@@ -24,3 +26,13 @@ def home_task(request):
     tasks = Task.objects.all()
     context = {'tasks':tasks}
     return render(request, 'index.html', context)
+
+def task_add(request):
+    if request.method == 'GET':
+        return render(request, 'add.html')
+    title = request.POST.get('title')
+    description = request.POST.get('description')
+    time = datetime.datetime.now()
+    task = Task(title=title, description=description, created_at=time)
+    task.save()
+    return redirect('/')
